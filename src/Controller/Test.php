e08d19f675c1457de51020707bfff808ed212590
smoers <serge.moers@mo-consult.be>
2020-01-15 14:11:47 +0100

    2020-01-15

--- src/Controller/Test.php
+++ src/Controller/Test.php
@@ -4,7 +4,18 @@
 namespace App\Controller;
 
 
-class Test
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Test extends  AbstractController
 {
+    /**
+     * @Route("/test", methods={"GET"})
+     */
+    public function testing()
+    {
+        return new Response('<html><body>Testing page</body></html>');
+    }
 
 }
